cc66bce54cf68c94b6c79bf15b76814e2b8cbfcd
Ademez <ademolaoladosu1@gmail.com>
2022-09-08 21:13:17 -0700

    me

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,2 @@
-#include <stdio.h>
-
-/**
- * main - Entry Point
- *
- * Description: Prints "with proper grammar, but the outcome is
- * piece of art,"
- *
- * Return: 0 (Success)
- */
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art, \n");
-
-	return (0);
-}
+#include<stdio.h>
+#include<un
