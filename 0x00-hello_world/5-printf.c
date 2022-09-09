9a0042d098248388b9c00698151e668ce3e4adf3
Ademez <ademolaoladosu1@gmail.com>
2022-09-08 21:28:19 -0700

    my lord

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,14 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Description: Prints "with proper grammar, but the outcome is a
+ * piece of art,"
+ *
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but theoutcome is a piece of art,\n);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	retuen (0);
+		return (0);
 }
