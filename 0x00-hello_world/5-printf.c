f1c61458eea24a23e01ff71295929cf3facb9840
Ademez <ademolaoladosu1@gmail.com>
2022-09-08 20:16:50 -0700

    my commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Prints "with proper grammar, but the outcome is a
+ * pieceof art,"
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	return(0);
+}
