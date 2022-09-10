a2c5c23fe5d59629d9eff70f370b20229b8a7860
Ademez <ademolaoladosu1@gmail.com>
2022-09-10 11:40:30 -0700

    my commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry Point
  *
- * Description: prints the size of various
+ * Description:  Prints the size of various
  * types on the computer it is compiled and run on.
  *
  * Return: 0 (Success)
@@ -11,7 +11,7 @@
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
-	printf("Size of a char: %d byte(s)\n", (int) sizeof(int));
+	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
