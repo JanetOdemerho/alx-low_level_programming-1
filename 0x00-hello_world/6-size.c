3570f74eae7357c07f02b38b990185d2f6c789b6
Ademez <ademolaoladosu1@gmail.com>
2022-09-10 11:15:57 -0700

    my commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,23 +3,18 @@
 /**
  * main - Entry Point
  *
- * Description: Prints the size of various
- * types of the computer it is compilled and run on.
+ * Description: prints the size of various
+ * types on the computer it is compiled and run on.
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
-	printf("size of a char: %d
-byte(s)\n", (int) sizeof(char));
-	printf("size of an int: %d
-bytes(s)\n",(int) sizeof(size of(int));
-	printf("size of a long int:
-%d bytes(s)\n", (int) sizeof(long int));
-	printf("
-Size of a long long int: %d byte(s)\n
-", (int) sizeof(long long int));
-	printf("size of a float: %d
-bytes(s)\n", (int) sizeof(float));
+	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
+	printf("Size of a char: %d byte(s)\n", (int) sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
 
 	return (0);
+}
