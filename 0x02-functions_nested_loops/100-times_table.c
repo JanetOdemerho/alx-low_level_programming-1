70eca54b707b4c342244ddc4dcb4b7283f5d8582
Ademez <ademolaoladosu1@gmail.com>
2022-09-13 18:06:27 -0700

    my commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,9 +26,9 @@ void print_times_table(int n)
 					_putchar(k + '0');
 				} else if (k >= 10 && k < 100)
 				{
-					putchar(',');
+					_putchar(',');
 					_putchar(' ');
-					putchar(' ');
+					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				} else if (k >= 100)
