4a36efbf5447c79e00aa5e40a535ea140076910e
Ademez <ademolaoladosu1@gmail.com>
2022-09-13 19:34:18 -0700

    my commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,7 +4,7 @@
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
  */
-print_times_table(int n)
+void print_times_table(int n)
 {
 	int i, j, k;
 
@@ -28,7 +28,7 @@ print_times_table(int n)
 				} else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
-					_putchar(' ');
+					_putchar(',');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
@@ -45,4 +45,3 @@ print_times_table(int n)
 		}
 	}
 }
-				
